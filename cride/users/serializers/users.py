2eb0815bca05545ac2b1540d9c771f8a069aa04f
Juan <jdrc2908@gmail.com>
2021-01-19 11:29:39 -0500

    signup function with serializer

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,14 +1,16 @@
 """ Users serializers. """
 
 # Django
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, password_validation
+from django.core.validators import RegexValidator
 
 # Django REST framework
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
 
 # Models
-from cride.users.models import User
+from cride.users.models import User, Profile
 
 class UserModelSerializer(serializers.ModelSerializer):
     """ User model serializer. """
@@ -46,3 +48,56 @@ class UserLoginSerializer(serializers.Serializer):
         """ Generate or retrieve new token. """
         token, created = Token.objects.get_or_create(user=self.context['user'])
         return self.context['user'], token.key
+
+class UserSignUpSerializer(serializers.Serializer):
+    """ User sign up serializer. 
+    
+        Handle sign up data validation and user/profile creation.
+    """
+
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    # username
+    username = serializers.CharField(
+        min_length = 4,
+        max_length = 20,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    # phone number
+    phone_regex = RegexValidator(
+        regex='\+?1?\d{8,15}$',
+        message='Phone number must be enterd in the format: +50769237843. Up to 15 digits allowed.'
+    )
+    phone_number = serializers.CharField(
+        validators=[phone_regex]
+    )
+
+    # password
+    password = serializers.CharField(min_length=8, max_length=64)
+    password_confirmation = serializers.CharField(min_length=8, max_length=64)
+
+    # name 
+
+    first_name = serializers.CharField(min_length=2, max_length=40)
+    last_name = serializers.CharField(min_length=2, max_length=40)
+
+    def validate(self, data):
+        """ Verify passwords match. """
+        passwd = data['password']
+        passwd_conf = data['password_confirmation']
+
+        if passwd != passwd_conf:
+            raise serializers.ValidationErrors("Password don't match.")
+
+        password_validation.validate_password(passwd)
+        return data
+
+    def create(self, data):
+        """ Handle user/profile creation. """
+
+        data.pop('password_confirmation')
+        user = User.objects.create_user(**data)
+        profile = Profile.objects.create(user=user)
+        return user
--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -8,7 +8,8 @@ from rest_framework import status
 # Serializers
 from cride.users.serializers import (
     UserLoginSerializer, 
-    UserModelSerializer
+    UserModelSerializer,
+    UserSignUpSerializer
     )
 
 class UserLoginAPIView(APIView):
@@ -23,4 +24,16 @@ class UserLoginAPIView(APIView):
             'user': UserModelSerializer(user).data,
             'token': token
         }
+        return Response(data, status=status.HTTP_201_CREATED)
+
+class UserSignUpAPIView(APIView):
+    """ User sign up API view. """
+
+    def post(self, request, *args, **kwargs):
+        """ Handle HTTP POST request. """
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
+          
         return Response(data, status=status.HTTP_201_CREATED)
